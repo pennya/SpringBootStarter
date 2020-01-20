714d98fd3b40b4b953494f47bc88a161bf598822
김지훈(Jihoon Kim)/App개발팀/11ST <jhkim077@sk.com>
2020-01-21 03:19:01 +0900

    톰캣 객체로 직접 서버 만들기

--- demoServer/src/main/kotlin/com/tunateam/demoServer/DemoServerApplication.kt
+++ demoServer/src/main/kotlin/com/tunateam/demoServer/DemoServerApplication.kt
@@ -1,9 +1,13 @@
 package com.tunateam.demoServer
 
 import com.tunateam.Holoman
+import org.apache.catalina.startup.Tomcat
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @SpringBootApplication
 class DemoServerApplication {
@@ -25,5 +29,32 @@ class DemoServerApplication {
 }
 
 fun main(args: Array<String>) {
-    runApplication<DemoServerApplication>(*args)
+    //runApplication<DemoServerApplication>(*args)
+
+
+    /**
+     * 스프링 부트 내장 서버 없이 톰캣 객체만 가지고 서버를 만듬.
+     * 이런 비슷한 설정이 AutoConfigure 와 관련이 있다.
+     */
+    val tomcat = Tomcat()
+    tomcat.setPort(8080)
+
+    val context = tomcat.addContext("/", "/")
+    val servlet = object: HttpServlet() {
+        override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+            resp.writer.apply {
+                println("<html><head><title>")
+                println("Hey, Tomcat")
+                println("</title></head>")
+                println("<body><h1>Hello Tomcat</h1></body>")
+                println("</html>")
+            }
+        }
+    }
+    val servletName = "helloServlet"
+    tomcat.addServlet("/", servletName, servlet)
+    context.addServletMappingDecoded("/hello", servletName)
+
+    tomcat.start()
+    tomcat.server.await()
 }
