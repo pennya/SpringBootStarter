9c09797eb9c3392dc27e9d454d43e1f7debb448c
pennya <rlawlgns077@gmail.com>
2020-02-09 00:51:17 +0900

    MockBean

--- SpringInit/src/test/java/com/duzi/SpringInit/SampleControllerTestForRandomPort.java
+++ SpringInit/src/test/java/com/duzi/SpringInit/SampleControllerTestForRandomPort.java
@@ -3,9 +3,11 @@ package com.duzi.SpringInit;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SampleControllerTestForRandomPort {
@@ -13,10 +15,15 @@ public class SampleControllerTestForRandomPort {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() {
+        when(mockSampleService.getName()).thenReturn("world");
+
         String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello springboot");
+        assertThat(result).isEqualTo("hello world");
     }
 
 }
