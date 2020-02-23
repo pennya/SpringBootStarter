0d679734c370010fbb648af5c3e26c5971a537c8
pennya <rlawlgns077@gmail.com>
2020-02-23 23:37:17 +0900

    jdbc h2 기본

---
+++ database/src/main/java/com/example/database/H2Runner.java
@@ -0,0 +1,23 @@
+package com.example.database;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection.getMetaData().getURL());
+        System.out.println(connection.getMetaData().getUserName());
+    }
+}
