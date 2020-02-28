1ac7b64864444aa7e83b98907923aa8b968547e6
김지훈(Jihoon Kim)/App개발팀/11ST <jhkim077@sk.com>
2020-02-28 19:27:28 +0900

    postgres 추가 및 테스트 코드에서 postgres db 사용

--- springbootjpa/src/test/java/com/duzi/springbootjpa/account/AccountRepositoryTest.java
+++ springbootjpa/src/test/java/com/duzi/springbootjpa/account/AccountRepositoryTest.java
@@ -3,13 +3,18 @@ package com.duzi.springbootjpa.account;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
 class AccountRepositoryTest {
 
     @Autowired
@@ -22,7 +27,12 @@ class AccountRepositoryTest {
     AccountRepository accountRepository;
 
     @Test
-    public void di() {
-
+    public void di() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getURL());
+            System.out.println(metaData.getDriverName());
+            System.out.println(metaData.getUserName());
+        }
     }
 }
