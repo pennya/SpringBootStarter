7c7b70fd10cc68cfe877ecf1af684da6460e7e7f
김지훈(Jihoon Kim)/App개발팀/11ST <jhkim077@sk.com>
2020-02-28 19:08:21 +0900

    jpa 추가 및 테스트 코드 실행

---
+++ springbootjpa/src/test/java/com/duzi/springbootjpa/SpringbootjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duzi.springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springbootjpa/src/test/java/com/duzi/springbootjpa/account/AccountRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.duzi.springbootjpa.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AccountRepositoryTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void di() {
+
+    }
+}
