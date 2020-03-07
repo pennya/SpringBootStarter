9beec3d029e40218786fabac2258f83ef81e6883
김지훈(Jihoon Kim)/App개발팀/11ST <jhkim077@sk.com>
2020-03-07 23:17:46 +0900

    NoOpPasswordEncoder 사용법

--- springbootsecurity2/src/main/java/com/example/springbootsecurity2/config/SecurityConfig.java
+++ springbootsecurity2/src/main/java/com/example/springbootsecurity2/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.springbootsecurity2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,5 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin() // 로그인 창을 통해서 할꺼고
                 .and()
                 .httpBasic(); // 기본 http basic 을 사용한다
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 패스워드 인코더를 사용하지 않는 방식으로 절대 사용하면 안됨. 빠르게 테스트 하기 위해서만 잠시 사용할 것
+        return NoOpPasswordEncoder.getInstance();
     }
 }
